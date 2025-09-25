00771e9ba81fd78009fcf6edee4705e7defff548
Pendragon Arturia <2144973442@qq.com>
2025-09-25 20:47:38 +0800

    交换链的check

--- DrawingaTriangle/DrawTriangle.cpp
+++ DrawingaTriangle/DrawTriangle.cpp
@@ -14,6 +14,9 @@
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+const std::vector<const char*> deviceExtensions = {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
 
 const std::vector<const char*> validationLayers = {
     "VK_LAYER_KHRONOS_validation"
@@ -296,7 +299,25 @@ private:
     bool isDeviceSuitable(VkPhysicalDevice device) {
 		QueueFamilyIndices indices = findQueueFamilies(device);
 
+		bool extensionsSupported = checkDeviceExtensionSuppoert(device);    
+
         return indices.isComplete();
+	}
+
+    bool checkDeviceExtensionSuppoert(VkPhysicalDevice device) {
+		uint32_t extensionCount;
+		vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+
+		std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+		vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, availableExtensions.data());
+
+		std::set<std::string> requiredExtensions(deviceExtensions.begin(), deviceExtensions.end());
+
+        for (const auto& extension : availableExtensions) {
+			requiredExtensions.erase(extension.extensionName);
+        }
+
+        return requiredExtensions.empty();
 	}
 
     QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
